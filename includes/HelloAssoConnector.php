c3c7b719fba587a29a421b31ea27321cb6837dda
Gerald Col <gnut@gnut.eu>
2024-05-31 14:46:33 +0200

    correctiif connexion et payments

--- includes/HelloAssoConnector.php
+++ includes/HelloAssoConnector.php
@@ -19,17 +19,17 @@ class HelloAssoConnector
         if (isset($_GET['order'])) {
             $endpoint = "/items/{$_GET['order']}/";
         } else {
-            if (isset($_GET['page']) && ($_GET['page'] === 'admin') && !isset($_GET['item'])) {
+            if (isset($_GET['page']) && ($_GET['page'] === 'admin') && !isset($_GET['item']) && !isset($_GET['payment'])) {
                 $endpoint = "/organizations/{$this->organizationSlug}/";
             } else {
                 if (isset($_GET['payment'])) {
-                    $endpoint = "/payments/{$_GET['payment']}/";
+                       $endpoint = "/payments/{$_GET['payment']}/";
                 } else {
                     $endpoint = $this->buildEndpoint();
                 }
             }
         }
-        
+
         return $this->apiHandler->getData($endpoint);
     }
 
@@ -39,11 +39,11 @@ class HelloAssoConnector
 
         // Check if the only GET parameter is 'page' and its value is 'admin'
         if (isset($_GET['item']) && $_GET['item'] === 'payments') {
-            $endpoint = "/organizations/{$this->organizationSlug}/payments?";
+            $endpoint = "/organizations/{$this->organizationSlug}/payments/search?";
         } else {
             $endpoint = "/organizations/{$this->organizationSlug}/items?";
         }
-        
+
         $params = [
             'pageIndex' => isset($_GET['pageIndex']) ? $_GET['pageIndex'] : 1,
             'pageSize' => isset($_GET['tierTypes']) ? 4 : 12,
--- includes/payments.php
+++ includes/payments.php
@@ -1,34 +1,38 @@
 <?php
-// require_once 'HelloAssoConnector.php';
+require_once 'HelloAssoConnector.php';
 
-// if (isset($_SESSION['bearer_token'])) {
-//     $connector = new HelloAssoConnector($_SESSION['bearer_token'], $organizationSlug);
-//     if (isset($_GET['payment'])) {
-//          $data_payment = $connector->fetchData();
-//     } else {
-//         $data_forms = $connector->fetchData();
-    
-//     }
-    // Vous pouvez maintenant utiliser $data_forms pour traiter les données récupérées
-    // var_dump($data_forms);
-    // die();
-// } else {
-//     die("Erreur: Jeton d'authentification manquant.");
-// }
-
-// include_once 'orders.php';
-if (isset($_GET['payment'])) {
-    $url = "https://api.helloasso.com/v5/payments/{$_GET['payment']}/";
+if (isset($_SESSION['bearer_token'])) {
+    $connector = new HelloAssoConnector($_SESSION['bearer_token'], $organizationSlug);
+    if (isset($_GET['payment'])) {
+        $url = "https://api.helloasso.com/v5/payments/{$_GET['payment']}/";
     $authorization = "Bearer " . $_SESSION['bearer_token'];
     $data_payment = curlApiGet($url, $authorization);
-} else {
-    if(!isset($_GET['formType'])) {
-        $url = "https://api.helloasso.com/v5/organizations/{$organizationSlug}/payments/search?pageSize=50&sortOrder=Desc&sortField=Date";
+    // $data_payment = $connector->fetchData();
+    //          var_dump($data_payment);
+    // die();
     } else {
-        $url = "https://api.helloasso.com/v5/organizations/{$organizationSlug}/payments/search?pageSize=50&formType={$_GET['formType']}&sortOrder=Desc&sortField=Date";
-    }
-    
-    $authorization = "Bearer " . $_SESSION['bearer_token'];
-    $data_forms = curlApiGet($url, $authorization);
+        $data_forms = $connector->fetchData();
+    //     var_dump($data_forms);
+    // die();
+    }    
 
+} else {
+    die("Erreur: Jeton d'authentification manquant.");
 }
+
+// include 'orders.php';
+// if (isset($_GET['payment'])) {
+//     $url = "https://api.helloasso.com/v5/payments/{$_GET['payment']}/";
+//     $authorization = "Bearer " . $_SESSION['bearer_token'];
+//     $data_payment = curlApiGet($url, $authorization);
+// } else {
+//     if(!isset($_GET['formType'])) {
+//         $url = "https://api.helloasso.com/v5/organizations/{$organizationSlug}/payments/search?pageSize=50&sortOrder=Desc&sortField=Date";
+//     } else {
+//         $url = "https://api.helloasso.com/v5/organizations/{$organizationSlug}/payments/search?pageSize=50&formType={$_GET['formType']}&sortOrder=Desc&sortField=Date";
+//     }
+    
+//     $authorization = "Bearer " . $_SESSION['bearer_token'];
+//     $data_forms = curlApiGet($url, $authorization);
+
+// }
